fee4c59086d0ecb2bc0d3b22d9d0b9ee3cbdc09b
Kai <kai.schuette@mailbox.org>
2025-11-06 16:02:38 +0100

    Remove unused function from sampling.py

--- process/utils/sampling.py
+++ process/utils/sampling.py
@@ -64,62 +64,6 @@ def _uvgrid_reverse_u(grid):
     return reversed_grid
 
 
-
-def uvgrid(face, given_bound=None,num_u=10, num_v=10, uvs=False, method="point", reverse_order_with_face=True):
-    """ 
-    Creates a 2D UV-grid of samples from the given face
-
-    Args:
-        face (occwl.face.Face): A B-rep face
-        num_u (int): Number of samples along u-direction. Defaults to 10.
-        num_v (int): Number of samples along v-direction. Defaults to 10.
-        uvs (bool): Return the surface UVs where quantities are evaluated. Defaults to False.
-        method (str): Name of the method in the occwl.face.Face object to be called 
-                      (the method has to accept the uv value as argument). Defaults to "point".
-    
-    Returns:
-        np.ndarray: 2D array of quantity evaluated on the face geometry
-        np.ndarray (optional): 2D array of uv-values where evaluation was done
-    """
-    assert num_u >= 2
-    assert num_v >= 2
-
-    if given_bound is None:
-        uv_box = uv_bounds(face)
-    else:
-        umin, umax, vmin, vmax = given_bound
-        uv_box = Box(np.array([umin, vmin]))
-        uv_box.encompass_point(np.array([umax, vmax]))
-
-    if method=='point':
-        fn=lambda uv:pointOnFace(face,uv)
-    elif method=='normal':
-        fn=lambda uv:normalOnFace(face,uv)
-    else:
-        fn = getattr(face, method)
-
-    uvgrid = []
-    uv_values = np.zeros((num_u, num_v, 2), dtype=np.float32)
-
-    for i in range(num_u):
-        u = uv_box.intervals[0].interpolate(float(i) / (num_u - 1))
-        for j in range(num_v):
-            v = uv_box.intervals[1].interpolate(float(j) / (num_v - 1))
-            uv = np.array([u, v])
-            uv_values[i, j] = uv
-            val = fn(uv)
-            uvgrid.append(val)
-    uvgrid = np.asarray(uvgrid).reshape((num_u, num_v, -1))
-
-    if reverse_order_with_face:
-        if face.reversed():
-            uvgrid = _uvgrid_reverse_u(uvgrid)
-            uv_values = _uvgrid_reverse_u(uv_values)
-
-    if uvs:
-        return uvgrid, uv_values
-    return uvgrid
-
 def ugrid(curve:Geom_BSplineCurve,u_range, num_u: int = 10,us=False, method="point", reverse_order_with_edge=True):
     """ 
     Creates a 1D UV-grid of samples from the given edge
